e8288d2b591e35841a225d6ebfadb50257916171
IGedeCahyaPurnamaYasa <cahya.purnama456@gmail.com>
2026-01-17 20:40:05 +0800

    offering - add tabs in list

--- app/Filament/Company/Resources/Common/OfferingResource/Pages/ListOfferings.php
+++ app/Filament/Company/Resources/Common/OfferingResource/Pages/ListOfferings.php
@@ -4,8 +4,10 @@ namespace App\Filament\Company\Resources\Common\OfferingResource\Pages;
 
 use App\Filament\Company\Resources\Common\OfferingResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Support\Enums\MaxWidth;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListOfferings extends ListRecords
 {
@@ -21,5 +23,23 @@ class ListOfferings extends ListRecords
     public function getMaxContentWidth(): MaxWidth | string | null
     {
         return 'max-w-8xl';
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'sellable' => Tab::make()
+                    ->label('Sellable')
+                    ->modifyQueryUsing(function(Builder $query) {
+                        $query->where('sellable', 1);
+                    }),
+            'purchasable' => Tab::make()
+                    ->label('Purchasable')
+                    ->modifyQueryUsing(function(Builder $query) {
+                        $query->where('purchasable', 1);
+                    }),
+            'all'   => Tab::make()
+                    ->label('All')
+        ];
     }
 }
